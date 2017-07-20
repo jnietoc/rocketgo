11e293905ca5dc8bd12db73a471fd0ed06acba15
Artemio Nieto <jancsistemas@gmail.com>
2017-07-19 22:36:46 -0500

    Add dependency

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,4 +1,16 @@
-
+environments {
+	production {
+		dataSource {
+			dbCreate = "create-drop"
+			driverClassName = "org.postgresql.Driver"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+			username = uri.userInfo.split(":")[0]
+			password = uri.userInfo.split(":")[1]
+		}
+	}
+}
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.rocket.go.seguridad.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.rocket.go.seguridad.UserRole'
